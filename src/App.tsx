1059b54e77bbeb129f50e403e2535ee2b46b046e
fadeyush <a.fadeyush@gmail.com>
2024-04-02 20:54:17 +0300

    визуальное добавление блока даты с текущей неделей

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './App.module.scss';
 import LineChart from './components/charts/LineChart';
 import MyCheckbox from './components/UI/checkbox/MyCheckbox';
@@ -12,6 +12,37 @@ function App() {
     {title: 'Доллар', name: 'usd'}, 
     {title: 'Юань', name: 'cny'}
   ];
+  const [startDay, setStartDay] = useState<string>('');
+  const [endDay, setEndDay] = useState<string>('');
+
+  useEffect(()=>{
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const monthDay = currentDate.getDate();
+    const weekDay = currentDate.getDay();
+
+    let result = {startDate: '', endDate: ''};
+    let countMonthDay;
+    if (weekDay > 1) {
+      countMonthDay = monthDay - (weekDay - 1);
+    } else if (weekDay === 0) {
+      countMonthDay = monthDay - 6;
+    } else {
+      countMonthDay = monthDay;
+    }
+  
+    let startDay = countMonthDay;
+    let endDay = countMonthDay + 6;
+    setStartDay(`${currentYear}-${currentDate.getMonth() < 10 ? `0${currentDate.getMonth()}`: currentDate.getMonth()}-${startDay < 10 ? `0${startDay}` : startDay}`);
+    setEndDay(`${currentYear}-${currentDate.getMonth() < 10 ? `0${currentDate.getMonth()}`: currentDate.getMonth()}-${endDay < 10 ? `0${endDay}` : endDay}`);
+  }, []);
+
+  const changeStartDay = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartDay(e.target.value);
+  }
+  const changeEndDay = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEndDay(e.target.value);
+  }
 
   return (
     <div className={classes.App}>
@@ -22,13 +53,20 @@ function App() {
 
           <ul className={classes.chart__currency}>
             {currencyArr.map(currency =>
-              <li className={classes.chart__currencyItem}>
-                <MyCheckbox key={currency.name} title={currency.title} name={currency.name}/>
+              <li key={currency.name} className={classes.chart__currencyItem}>
+                <MyCheckbox title={currency.title} name={currency.name}/>
               </li>
             )} 
           </ul>
 
-          <ul className={classes.chart__date}></ul>
+          <ul className={classes.chart__date}>
+            <li>
+              <input type="date" value={startDay} onChange={changeStartDay}/>
+            </li>
+            <li>
+              <input type="date" value={endDay} onChange={changeEndDay}/>
+            </li>
+          </ul>
         </div>
         <LineChart/>
 
